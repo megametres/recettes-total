e081713f6131b18a8b7c90686e5916a75ba83da8
Daniel Sauvé <megametres@users.noreply.github.com>
2020-10-11 00:33:33 -0400

    fix var use

--- docker/cross-armv7/build-armv7-rust-binary.sh
+++ docker/cross-armv7/build-armv7-rust-binary.sh
@@ -24,12 +24,12 @@ fi
 # Set the image in Cross.toml to $docker_tag_cross
  sed -i "s/""$docker_tag_cross_postgres""/""$docker_tag_cross""/g" ../../api/Cross.toml
  # First compilation that will fail
- docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(dirname $(dirname `pwd`))/api:/code cross-pi-runner cross build --target=arm-unknown-linux-gnueabihf --release
+ docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(dirname $(dirname `pwd`))/api:/code $docker_tag_runner cross build --target=arm-unknown-linux-gnueabihf --release
 
 # Set the image in Cross.toml to $docker_tag_cross
  sed -i "s/""$docker_tag_cross""/""$docker_tag_cross_postgres""/g" ../../api/Cross.toml
  # Second compilation that will passes
- docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(dirname $(dirname `pwd`))/api:/code cross-pi-runner cross build --target=arm-unknown-linux-gnueabihf --release
+ docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(dirname $(dirname `pwd`))/api:/code $docker_tag_runner cross build --target=arm-unknown-linux-gnueabihf --release
 
 # Copy the binary
  cp ../../api/target/arm-unknown-linux-gnueabihf/release/recipes_api dist/
