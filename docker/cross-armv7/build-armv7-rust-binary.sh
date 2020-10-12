85d2a8d9fe00a2a487ca6fea8b77e130f95d9e8a
Daniel Sauvé <megametres@users.noreply.github.com>
2020-10-11 21:04:23 -0400

    Update nightly + build as a user

--- docker/cross-armv7/build-armv7-rust-binary.sh
+++ docker/cross-armv7/build-armv7-rust-binary.sh
@@ -12,7 +12,7 @@ if [[ -z $( docker images -q ${docker_tag_cross_postgres} ) ]]; then
   docker build -t ${docker_tag_cross_postgres} . --build-arg POSTGRES=true
 fi
 if [[ -z $( docker images -q ${docker_tag_runner} ) ]]; then
-  docker build . -t ${docker_tag_runner} --file Dockerfile-cross-runner
+  docker build . -t ${docker_tag_runner} --file Dockerfile-cross-runner --build-arg DOCKER_GID=$(cut -d: -f3 < <(getent group docker))
 fi
 
 # Remove old builds
@@ -21,12 +21,12 @@ fi
 # Set the image in Cross.toml to $docker_tag_cross
  sed -i "s/""$docker_tag_cross_postgres""/""$docker_tag_cross""/g" ../../api/Cross.toml
  # First compilation that will fail
- docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(dirname $(dirname `pwd`))/api:/code $docker_tag_runner cross build --target=arm-unknown-linux-gnueabihf --release
+ docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(dirname $(dirname `pwd`))/api:/home/runner/code $docker_tag_runner cross build --target=arm-unknown-linux-gnueabihf --release
 
 # Set the image in Cross.toml to $docker_tag_cross
  sed -i "s/""$docker_tag_cross""/""$docker_tag_cross_postgres""/g" ../../api/Cross.toml
  # Second compilation that will passes
- docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(dirname $(dirname `pwd`))/api:/code $docker_tag_runner cross build --target=arm-unknown-linux-gnueabihf --release
+ docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(dirname $(dirname `pwd`))/api:/home/runner/code $docker_tag_runner cross build --target=arm-unknown-linux-gnueabihf --release
 
 # Copy the binary
  cp ../../api/target/arm-unknown-linux-gnueabihf/release/recipes_api dist/
